bae153d0e73a2886c54d62ae7b2f5514915f8677
PRAJAPATI HENIL <153110063+Henil-Prajapati@users.noreply.github.com>
2024-09-03 00:49:32 +0530

    Add files via upload

---
+++ Common/footer.php
@@ -0,0 +1,44 @@
+footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-one md-mb-30 sm-mb-30">
+            <h3>Dharamsinh Desai University</h3>
+            <p>This Project has been made in DUHacks3.0 with the help of 
+              my GDSC mentors.
+              The prupose of this Project to learn new skills & explore some tech in current industry.
+            </p>
+          </div>
+          <div class="col-md-2 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>Quick Links</h2>
+            <ul>
+              <li><a href="">About DDU</a></li>
+              <li><a href="">Admissions Policy</a></li>
+              <li><a href="">Apply for Admission</a></li>
+              <li><a href="">Download Prospectus</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>UNDERGRADUATE PROGRAMS</h2>
+            <ul>
+              <li><a href="">B.tech Civil Engineering</a></li>
+              <li><a href="">B.tech Electrical Engineering</a></li>
+              <li><a href="">B.tech Mechanical Engineering</a></li>
+              <li><a href="">B.tech Information Technology</a></li>
+              <li><a href="">B.tech Computer Engineering</a></li>
+              <li><a href="">B.tech Chemical Engineering</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p>Address: Dharamsinh Desai University,
+              College Road, Nadiad 387 001, Gujarat, India.</p>
+              <p>Telephones:	Phone: +91 268 2520502</p>
+              <p>E-mail:  registrar@ddu.ac.in</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  <p class="footer-bottom-text">Copyright © 2023 DHARMSINH DESAI UNIVERSITY. All rights reserved. 2024</p>
+</footer>
---
+++ Connection/connection.php
@@ -0,0 +1,7 @@
+<?php 
+	$con=mysqli_connect("localhost","root","","DDU_college");
+	if(!$con)
+	{
+		echo "Connection is not Successfully";
+	}
+?>
